f09855fb21326d35c2692748cf5ba0d494eaa368
janvigambhir28-eng <janvigambhir28@gmail.com>
2026-06-23 18:05:52 +0530

    Add files via upload

---
+++ 2_SimpleInterest.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+// create the buckets
+    int p,r,t;
+
+    float si;
+
+
+
+/*cin>>p;
+
+    cin>>r;
+
+    cin>>t;*/
+
+
+// take input from tne user
+    cin>>p>>r>>t;
+
+    
+
+
+
+
+//3. calculate
+    si=(p*r*t)/100.0;
+//4. output your answer
+    cout<<"simple interest is " << si <<endl;
+
+
+
+
+
+    return 0;
+
+}
