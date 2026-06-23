277819f6adb2784dedf9208c61cba1e7ba9b258c
janvigambhir28-eng <janvigambhir28@gmail.com>
2026-06-23 18:04:06 +0530

    Add files via upload

---
+++ 1_Helloworld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>// header file- code to take input and output 
+using namespace std;// mandatory to write 
+
+int main(){
+
+    cout<<"Hello world";// in cpp every statement ends with a semicolon//cout==console output 
+    return 0;// zero is status bit (successful execution)
+}
+ 
